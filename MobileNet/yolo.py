a622ceff1a802de87e08c8e767d5edc673d1d6e5
Behnam Yousefimehr <75952996+behnamy2010@users.noreply.github.com>
2023-07-18 21:30:32 +0330

    Add files via upload

---
+++ MobileNet/yolo.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from yolov6.layers.common import *
+from yolov6.utils.torch_utils import initialize_weights
+from yolov6.models.efficientrep import *
+from yolov6.models.reppan import *
+from yolov6.utils.events import LOGGER
+
+
+class Model(nn.Module):
+    export = False
+    '''YOLOv6 model with backbone, neck and head.
+    The default parts are EfficientRep Backbone, Rep-PAN and
+    Efficient Decoupled Head.
+    '''
+    def __init__(self, config, channels=3, num_classes=None, fuse_ab=False, distill_ns=False):  # model, input channels, number of classes
+        super().__init__()
+        # Build network
+        num_layers = config.model.head.num_layers
+        self.backbone, self.neck, self.detect = build_network(config, channels, num_classes, num_layers, fuse_ab=fuse_ab, distill_ns=distill_ns)
+
+        # Init Detect head
+        self.stride = self.detect.stride
+        self.detect.initialize_biases()
+
+        # Init weights
+        initialize_weights(self)
+
+    def forward(self, x):
+        export_mode = torch.onnx.is_in_onnx_export() or self.export
+        x = self.backbone(x)
+        x = self.neck(x)
+        if not export_mode:
+            featmaps = []
+            featmaps.extend(x)
+        x = self.detect(x)
+        return x if export_mode is True else [x, featmaps]
+
+    def _apply(self, fn):
+        self = super()._apply(fn)
+        self.detect.stride = fn(self.detect.stride)
+        self.detect.grid = list(map(fn, self.detect.grid))
+        return self
+
+def conv_bn2(inp, oup, stride):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 3, stride, 1, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU6(inplace=True)
+    )
+
+
+def conv_1x1_bn2(inp, oup):
+    return nn.Sequential(
+        nn.Conv2d(inp, oup, 1, 1, 0, bias=False),
+        nn.BatchNorm2d(oup),
+        nn.ReLU6(inplace=True)
+    )
+
+
+def make_divisible2(x, divisible_by=8):
+    import numpy as np
+    return int(np.ceil(x * 1. / divisible_by) * divisible_by)
+
+
+class InvertedResidual(nn.Module):
+    def __init__(self, inp, oup, stride, expand_ratio):
+        super(InvertedResidual, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        hidden_dim = int(inp * expand_ratio)
+        self.use_res_connect = self.stride == 1 and inp == oup
+
+        if expand_ratio == 1:
+            self.conv = nn.Sequential(
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+        else:
+            self.conv = nn.Sequential(
+                # pw
+                nn.Conv2d(inp, hidden_dim, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # dw
+                nn.Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups=hidden_dim, bias=False),
+                nn.BatchNorm2d(hidden_dim),
+                nn.ReLU6(inplace=True),
+                # pw-linear
+                nn.Conv2d(hidden_dim, oup, 1, 1, 0, bias=False),
+                nn.BatchNorm2d(oup),
+            )
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class MobileNetV2(nn.Module):
+    def __init__(self, n_class=1000, input_size=224, width_mult=1.):
+        super(MobileNetV2, self).__init__()
+        block = InvertedResidual
+        input_channel = 32
+        last_channel = 1280
+        interverted_residual_setting = [
+            # t, c, n, s
+            [1, 16, 1, 1],
+            [6, 24, 2, 2],
+            [6, 32, 3, 2],
+            [6, 64, 4, 2],
+            [6, 96, 3, 1],
+            [6, 160, 3, 2],
+            [6, 320, 1, 1],
+        ]
+
+        # building first layer
+        assert input_size % 32 == 0
+        # input_channel = make_divisible(input_channel * width_mult)  # first channel is always 32!
+        self.last_channel = make_divisible2(last_channel * width_mult) if width_mult > 1.0 else last_channel
+        self.features = [conv_bn2(3, input_channel, 2)]
+        # building inverted residual blocks
+        for t, c, n, s in interverted_residual_setting:
+            output_channel = make_divisible2(c * width_mult) if t > 1 else c
+            for i in range(n):
+                if i == 0:
+                    self.features.append(block(input_channel, output_channel, s, expand_ratio=t))
+                else:
+                    self.features.append(block(input_channel, output_channel, 1, expand_ratio=t))
+                input_channel = output_channel
+        # building last several layers
+        self.features.append(conv_1x1_bn2(input_channel, self.last_channel))
+        # make it nn.Sequential
+        self.features = nn.Sequential(*self.features)
+
+        # building classifier
+        #self.classifier = nn.Linear(self.last_channel, n_class)
+
+        self._initialize_weights()
+
+    def forward(self, x):
+        x1 = x
+        i = 0
+        #print(len(self.features))
+        outputs = []
+        for features_i in range(len(self.features)):
+            #print(features_i)
+            x1 = self.features[features_i](x1)
+            outputs.append(x1)
+        x = self.features(x)
+        #x = x.mean(3).mean(2)
+        #for tensor in outputs:
+            #print("Tensor size:", tensor.size())
+        #x = self.classifier(x)
+        indx1 = outputs[3]
+        indx2 = outputs[6]
+        indx3 = outputs[13]
+        indx4 = outputs[18]
+        outputs_final = []
+        outputs_final.append(indx1)
+        outputs_final.append(indx2)
+        outputs_final.append(indx3)
+        outputs_final.append(indx4)
+        return tuple(outputs_final)
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+            elif isinstance(m, nn.Linear):
+                n = m.weight.size(1)
+                m.weight.data.normal_(0, 0.01)
+                m.bias.data.zero_()
+
+
+def mobilenet_v2(pretrained=True):
+    model = MobileNetV2(width_mult=1)
+
+    if pretrained:
+        try:
+            from torch.hub import load_state_dict_from_url
+        except ImportError:
+            from torch.utils.model_zoo import load_url as load_state_dict_from_url
+        state_dict = load_state_dict_from_url(
+            'https://www.dropbox.com/s/47tyzpofuuyyv1b/mobilenetv2_1.0-f2a8633.pth.tar?dl=1', progress=True)
+        # Remove the 'classifier.1.weight' key from the dictionary
+        del state_dict['classifier.weight']
+        del state_dict['classifier.bias']
+        model.load_state_dict(state_dict)
+    return model
+
+
+
+
+def make_divisible(x, divisor):
+    # Upward revision the value x to make it evenly divisible by the divisor.
+    return math.ceil(x / divisor) * divisor
+
+
+def build_network(config, channels, num_classes, num_layers, fuse_ab=False, distill_ns=False):
+    depth_mul = config.model.depth_multiple
+    width_mul = config.model.width_multiple
+    num_repeat_backbone = config.model.backbone.num_repeats
+    channels_list_backbone = config.model.backbone.out_channels
+    fuse_P2 = config.model.backbone.get('fuse_P2')
+    cspsppf = config.model.backbone.get('cspsppf')
+    num_repeat_neck = config.model.neck.num_repeats
+    channels_list_neck = config.model.neck.out_channels
+    use_dfl = config.model.head.use_dfl
+    reg_max = config.model.head.reg_max
+    num_repeat = [(max(round(i * depth_mul), 1) if i > 1 else i) for i in (num_repeat_backbone + num_repeat_neck)]
+    channels_list = [make_divisible(i * width_mul, 8) for i in (channels_list_backbone + channels_list_neck)]
+
+    block = get_block(config.training_mode)
+    BACKBONE = eval(config.model.backbone.type)
+    NECK = eval(config.model.neck.type)
+
+    if 'CSP' in config.model.backbone.type:
+
+        if "stage_block_type" in config.model.backbone:
+            stage_block_type = config.model.backbone.stage_block_type
+        else:
+            stage_block_type = "BepC3"  #default
+
+        backbone = BACKBONE(
+            in_channels=channels,
+            channels_list=channels_list,
+            num_repeats=num_repeat,
+            block=block,
+            csp_e=config.model.backbone.csp_e,
+            fuse_P2=fuse_P2,
+            cspsppf=cspsppf,
+            stage_block_type=stage_block_type
+        )
+
+        neck = NECK(
+            channels_list=channels_list,
+            num_repeats=num_repeat,
+            block=block,
+            csp_e=config.model.neck.csp_e,
+            stage_block_type=stage_block_type
+        )
+    else:
+        backbone = BACKBONE(
+            in_channels=channels,
+            channels_list=channels_list,
+            num_repeats=num_repeat,
+            block=block,
+            fuse_P2=fuse_P2,
+            cspsppf=cspsppf
+        )
+
+        neck = NECK(
+            channels_list=channels_list,
+            num_repeats=num_repeat,
+            block=block
+        )
+
+    if distill_ns:
+        from yolov6.models.heads.effidehead_distill_ns import Detect, build_effidehead_layer
+        if num_layers != 3:
+            LOGGER.error('ERROR in: Distill mode not fit on n/s models with P6 head.\n')
+            exit()
+        head_layers = build_effidehead_layer(channels_list, 1, num_classes, reg_max=reg_max)
+        head = Detect(num_classes, num_layers, head_layers=head_layers, use_dfl=use_dfl)
+
+    elif fuse_ab:
+        from yolov6.models.heads.effidehead_fuseab import Detect, build_effidehead_layer
+        anchors_init = config.model.head.anchors_init
+        head_layers = build_effidehead_layer(channels_list, 3, num_classes, reg_max=reg_max, num_layers=num_layers)
+        head = Detect(num_classes, anchors_init, num_layers, head_layers=head_layers, use_dfl=use_dfl)
+
+    else:
+        from yolov6.models.effidehead import Detect, build_effidehead_layer
+        head_layers = build_effidehead_layer(channels_list, 1, num_classes, reg_max=reg_max, num_layers=num_layers)
+        head = Detect(num_classes, num_layers, head_layers=head_layers, use_dfl=use_dfl)
+    # Create a new instance of the modified MobileNetV2 model
+    backbone = mobilenet_v2(True)
+    # Freeze the parameters of the new model
+    for param in backbone.parameters():
+        param.requires_grad = False
+    #backbone = SqueezeNet(num_classes=15)
+    return backbone, neck, head
+
+
+def build_model(cfg, num_classes, device, fuse_ab=False, distill_ns=False):
+    model = Model(cfg, channels=3, num_classes=num_classes, fuse_ab=fuse_ab, distill_ns=distill_ns).to(device)
+    return model
---
+++ SqueezeNet v1.1/yolo.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from yolov6.layers.common import *
+from yolov6.utils.torch_utils import initialize_weights
+from yolov6.models.efficientrep import *
+from yolov6.models.reppan import *
+from yolov6.utils.events import LOGGER
+from torch.hub import load_state_dict_from_url
+
+class Model(nn.Module):
+    export = False
+    '''YOLOv6 model with backbone, neck and head.
+    The default parts are EfficientRep Backbone, Rep-PAN and
+    Efficient Decoupled Head.
+    '''
+    def __init__(self, config, channels=3, num_classes=None, fuse_ab=False, distill_ns=False):  # model, input channels, number of classes
+        super().__init__()
+        # Build network
+        num_layers = config.model.head.num_layers
+        self.backbone, self.neck, self.detect = build_network(config, channels, num_classes, num_layers, fuse_ab=fuse_ab, distill_ns=distill_ns)
+
+        # Init Detect head
+        self.stride = self.detect.stride
+        self.detect.initialize_biases()
+
+        # Init weights
+        initialize_weights(self)
+
+    def forward(self, x):
+        export_mode = torch.onnx.is_in_onnx_export() or self.export
+        x = self.backbone(x)
+        x = self.neck(x)
+        if not export_mode:
+            featmaps = []
+            featmaps.extend(x)
+        x = self.detect(x)
+        return x if export_mode is True else [x, featmaps]
+
+    def _apply(self, fn):
+        self = super()._apply(fn)
+        self.detect.stride = fn(self.detect.stride)
+        self.detect.grid = list(map(fn, self.detect.grid))
+        return self
+
+class FireModule(nn.Module):
+    def __init__(self, inplanes: int, squeeze_planes: int, expand1x1_planes: int, expand3x3_planes: int) -> None:
+        super().__init__()
+        self.inplanes = inplanes
+        self.squeeze = nn.Conv2d(inplanes, squeeze_planes, kernel_size=1)
+        self.squeeze_activation = nn.ReLU(inplace=True)
+        self.expand1x1 = nn.Conv2d(squeeze_planes, expand1x1_planes, kernel_size=1)
+        self.expand1x1_activation = nn.ReLU(inplace=True)
+        self.expand3x3 = nn.Conv2d(squeeze_planes, expand3x3_planes, kernel_size=3, padding=1)
+        self.expand3x3_activation = nn.ReLU(inplace=True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.squeeze_activation(self.squeeze(x))
+        return torch.cat(
+            [self.expand1x1_activation(self.expand1x1(x)), self.expand3x3_activation(self.expand3x3(x))], 1
+        )
+class SqueezeNet(nn.Module):
+    def __init__(self, version: str = "1_0", num_classes: int = 1000, dropout: float = 0.5) -> None:
+        super().__init__()
+        self.num_classes = num_classes
+
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True, padding=1),
+            FireModule(64, 16, 64, 64),
+            FireModule(128, 16, 64, 64),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            FireModule(128, 32, 128, 128),
+            FireModule(256, 32, 128, 128),
+            nn.MaxPool2d(kernel_size=3, stride=2, ceil_mode=True),
+            FireModule(256, 48, 192, 192),
+            FireModule(384, 48, 192, 192),
+            FireModule(384, 64, 256, 256),
+            FireModule(512, 64, 256, 256),
+        )
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x1 = x
+        print(len(self.features))
+        outputs = []
+        for features_i in range(len(self.features)):
+            x1 = self.features[features_i](x1)
+            outputs.append(x1)
+        x = self.features(x)
+        for tensor in outputs:
+            print("Tensor size:", tensor.size())
+        #x = self.classifier(x)
+        indx1 = outputs[0]
+        indx2 = outputs[3]
+        indx3 = outputs[6]
+        indx4 = outputs[12]
+        outputs_final = []
+        outputs_final.append(indx1)
+        outputs_final.append(indx2)
+        outputs_final.append(indx3)
+        outputs_final.append(indx4)
+        return tuple(outputs_final)
+
+
+def make_divisible(x, divisor):
+    # Upward revision the value x to make it evenly divisible by the divisor.
+    return math.ceil(x / divisor) * divisor
+
+
+def build_network(config, channels, num_classes, num_layers, fuse_ab=False, distill_ns=False):
+    depth_mul = config.model.depth_multiple
+    width_mul = config.model.width_multiple
+    num_repeat_backbone = config.model.backbone.num_repeats
+    channels_list_backbone = config.model.backbone.out_channels
+    fuse_P2 = config.model.backbone.get('fuse_P2')
+    cspsppf = config.model.backbone.get('cspsppf')
+    num_repeat_neck = config.model.neck.num_repeats
+    channels_list_neck = config.model.neck.out_channels
+    use_dfl = config.model.head.use_dfl
+    reg_max = config.model.head.reg_max
+    num_repeat = [(max(round(i * depth_mul), 1) if i > 1 else i) for i in (num_repeat_backbone + num_repeat_neck)]
+    channels_list = [make_divisible(i * width_mul, 8) for i in (channels_list_backbone + channels_list_neck)]
+
+    block = get_block(config.training_mode)
+    BACKBONE = eval(config.model.backbone.type)
+    NECK = eval(config.model.neck.type)
+
+    if 'CSP' in config.model.backbone.type:
+
+        if "stage_block_type" in config.model.backbone:
+            stage_block_type = config.model.backbone.stage_block_type
+        else:
+            stage_block_type = "BepC3"  #default
+
+        backbone = BACKBONE(
+            in_channels=channels,
+            channels_list=channels_list,
+            num_repeats=num_repeat,
+            block=block,
+            csp_e=config.model.backbone.csp_e,
+            fuse_P2=fuse_P2,
+            cspsppf=cspsppf,
+            stage_block_type=stage_block_type
+        )
+
+        neck = NECK(
+            channels_list=channels_list,
+            num_repeats=num_repeat,
+            block=block,
+            csp_e=config.model.neck.csp_e,
+            stage_block_type=stage_block_type
+        )
+    else:
+        backbone = BACKBONE(
+            in_channels=channels,
+            channels_list=channels_list,
+            num_repeats=num_repeat,
+            block=block,
+            fuse_P2=fuse_P2,
+            cspsppf=cspsppf
+        )
+
+        neck = NECK(
+            channels_list=channels_list,
+            num_repeats=num_repeat,
+            block=block
+        )
+
+    if distill_ns:
+        from yolov6.models.heads.effidehead_distill_ns import Detect, build_effidehead_layer
+        if num_layers != 3:
+            LOGGER.error('ERROR in: Distill mode not fit on n/s models with P6 head.\n')
+            exit()
+        head_layers = build_effidehead_layer(channels_list, 1, num_classes, reg_max=reg_max)
+        head = Detect(num_classes, num_layers, head_layers=head_layers, use_dfl=use_dfl)
+
+    elif fuse_ab:
+        from yolov6.models.heads.effidehead_fuseab import Detect, build_effidehead_layer
+        anchors_init = config.model.head.anchors_init
+        head_layers = build_effidehead_layer(channels_list, 3, num_classes, reg_max=reg_max, num_layers=num_layers)
+        head = Detect(num_classes, anchors_init, num_layers, head_layers=head_layers, use_dfl=use_dfl)
+
+    else:
+        from yolov6.models.effidehead import Detect, build_effidehead_layer
+        head_layers = build_effidehead_layer(channels_list, 1, num_classes, reg_max=reg_max, num_layers=num_layers)
+        head = Detect(num_classes, num_layers, head_layers=head_layers, use_dfl=use_dfl)
+
+    # Create an instance of the SqueezeNet model
+    backbone = SqueezeNet(num_classes=15)
+
+    # Loading Weights (Optional)
+    #state_dict = load_state_dict_from_url(
+    #            'https://download.pytorch.org/models/squeezenet1_1-b8a52dc0.pth', progress=True)
+    # Remove the 'classifier.1.weight' key from the dictionary
+    #del state_dict['classifier.1.weight']
+    #del state_dict['classifier.1.bias']
+    #backbone.load_state_dict(state_dict)
+    # Freeze the parameters of the new model
+    #for param in backbone.parameters():
+    #    param.requires_grad = False
+    return backbone, neck, head
+
+
+def build_model(cfg, num_classes, device, fuse_ab=False, distill_ns=False):
+    model = Model(cfg, channels=3, num_classes=num_classes, fuse_ab=fuse_ab, distill_ns=distill_ns).to(device)
+    return model
